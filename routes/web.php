3ff329904bd83460184e90474546faf09f9955df
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-02-15 00:54:00 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/home',[HomeController::class,'redirect']);
+Route::get('/',[HomeController::class,'index']);
+
+Route::middleware('can:User')->group(function(){
+    
+    Route::get('/request',[HomeController::class,'request']);
+    Route::post('/upload_request',[HomeController::class,'upload']);
+    Route::get('/viewstatus',[HomeController::class,'viewstatus']);
+    Route::get('/deleterequest/{id}',[HomeController::class,'deleterequest']);
+
+
+
+});
+
+Route::middleware('can:Admin')->group(function(){
+    Route::get('/daftar',[AdminController::class,'daftar']);
+    Route::post('/addengineer',[AdminController::class,'addengineer']);
+    Route::get('/engineerlist',[AdminController::class,'engineerlist']);
+    Route::get('/deleteengineer/{id}',[AdminController::class,'deleteengineer']);
+    Route::get('/updateengineer/{id}',[AdminController::class,'updateengineer']);
+    Route::post('/editengineer/{id}',[AdminController::class,'editengineer']);
+
+
+});
+
+
+
+
+
+
+
