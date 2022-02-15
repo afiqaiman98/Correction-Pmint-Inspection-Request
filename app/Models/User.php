fcf3782ab964acbec41ac3ee4d1eb13ec9def76a
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-02-16 01:47:27 +0800

    user view

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'usertype',
     ];
 
     /**
@@ -41,4 +42,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function hasRole($role)
+    {
+        if($this->usertype == $role){
+            return true;
+        }
+
+        return false;
+    }
 }
