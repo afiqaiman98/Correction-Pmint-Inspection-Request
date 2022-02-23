1fede0132f02891a9b4edcf4f5b3a67bbfc2a65e
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-02-23 22:25:56 +0800

    frontend issue select engineer

--- resources/views/user/inspect/request.blade.php
+++ resources/views/user/inspect/request.blade.php
@@ -1,7 +1,5 @@
 @include('user.head')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
 
@@ -75,10 +73,8 @@
 
             {{-- To choose Enginner for futher deveploment --}}
             <div class="form-group">
-
-
                 <label>Engineer For Inspection</label>
-                <select name="engineer" class="form-select">
+                <select name="engineer" class="form-control" style="height: auto!important">
 
                     <option class="hidden">-Select Engineer-</option>
                     @foreach ($engineers as $engineer)
