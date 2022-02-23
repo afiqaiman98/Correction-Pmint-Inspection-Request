e9eb33dcff029d18391b203ed6d418961037cdd3
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-02-23 11:25:01 +0800

    Fix select option

--- resources/views/user/inspect/request.blade.php
+++ resources/views/user/inspect/request.blade.php
@@ -1,5 +1,7 @@
 @include('user.head')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
 
